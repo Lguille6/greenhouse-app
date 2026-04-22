ff9390658b6efe2772da2edd9de12a512cd71b16
Leslie Guillen <lguille6@stu.southtexascollege.edu>
2026-04-22 01:28:45 -0500

    added garden bed

---
+++ src/components/GardenBed.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="garden-grid">
+    <div v-for="plant in store.plants" :key="plant.id" class="plant-card">
+      <h4>{{ plant.name }}</h4>
+      <p>Hydration: {{ plant.waterLevel }}%</p>
+      <button @click="store.waterPlant(plant.id)">Water</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useGardenStore } from "../stores/gardenStore";
+
+const store = useGardenStore();
+</script>
