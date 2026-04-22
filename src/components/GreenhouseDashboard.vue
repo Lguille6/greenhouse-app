46bdc804a8dc2bf2c9aab7517000f82e8f2750db
Leslie Guillen <lguille6@stu.southtexascollege.edu>
2026-04-22 01:39:18 -0500

    added greenhouse

---
+++ src/components/GreenhouseDashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dashboard">
+    <h2>Greenhouse Status</h2>
+    <p>
+      Overall Health: <strong>{{ store.gardenHealth }}%</strong>
+    </p>
+    <p v-if="store.thirstyPlantsCount > 0" style="color: orange">
+      Warning: {{ store.thirstyPlantsCount }} plants need water!
+    </p>
+    <p v-else>All plants are happy!</p>
+  </div>
+</template>
+
+<script setup>
+import { useGardenStore } from "../stores/gardenStore";
+const store = useGardenStore();
+</script>
