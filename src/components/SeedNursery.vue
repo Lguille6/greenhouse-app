32a09a4d224cacf8c03f5b307c86e39c31c37179
Leslie Guillen <lguille6@stu.southtexascollege.edu>
2026-04-22 01:22:47 -0500

    added seed nursery

---
+++ src/components/SeedNursery.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="panel">
+    <input v-model="plantName" placeholder="Name your plant..." />
+    <button @click="handlePlanting">Plant Seed</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useGardenStore } from "../stores/gardenStore";
+
+const plantName = ref("");
+const store = useGardenStore();
+
+const handlePlanting = () => {
+  if (plantName.value) {
+    store.plantSeed(plantName.value);
+    plantName.value = "";
+  }
+};
+</script>
